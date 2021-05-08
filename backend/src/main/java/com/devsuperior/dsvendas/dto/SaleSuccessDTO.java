3db7e58bbc78ac8a7cd0395374b0498c162c4188
Glaucia <glaucia.ap1258@gmail.com>
2021-05-08 14:15:48 -0300

    Group by search

---
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SaleSuccessDTO.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dsvendas.dto;
+
+import com.devsuperior.dsvendas.entities.Seller;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+public class SaleSuccessDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String sellerName;
+    private Long visited;
+    private Long deals;
+
+    public SaleSuccessDTO(Seller seller, Long visited, Long deals) {
+        sellerName = seller.getName();
+        this.visited = visited;
+        this.deals = deals;
+    }
+}
--- backend/src/main/java/com/devsuperior/dsvendas/service/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsvendas/service/SaleService.java
@@ -1,6 +1,8 @@
 package com.devsuperior.dsvendas.service;
 
 import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.dto.SaleSuccessDTO;
+import com.devsuperior.dsvendas.dto.SaleSumDTO;
 import com.devsuperior.dsvendas.entities.Sale;
 import com.devsuperior.dsvendas.repositories.SaleRepository;
 import com.devsuperior.dsvendas.repositories.SellerRepository;
@@ -9,6 +11,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class SaleService {
@@ -26,5 +30,14 @@ public class SaleService {
         return result.map(SaleDTO::new);
     }
 
+    @Transactional
+    public List<SaleSumDTO> amountGroupedBySeller() {
+        return saleRepository.amountGroupedBySeller();
+    }
+
+    @Transactional
+    public List<SaleSuccessDTO> successGroupedBySeller() {
+        return saleRepository.successGroupedBySeller();
+    }
 
 }
