8345cf19b57db34293ecc708ceff9c48f0351fd9
Glaucia <glaucia.ap1258@gmail.com>
2021-05-08 13:47:33 -0300

    Pagination

---
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SaleDTO.java
@@ -0,0 +1,31 @@
+package com.devsuperior.dsvendas.dto;
+
+import com.devsuperior.dsvendas.entities.Sale;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SaleDTO {
+
+    private Long id;
+    private Integer visited;
+    private Integer deals;
+    private Double amount;
+    private LocalDate date;
+
+    private SellerDTO seller;
+
+    public SaleDTO(Sale sale) {
+        id = sale.getId();
+        visited = sale.getVisited();
+        deals = sale.getDeals();
+        amount = sale.getAmount();
+        date = sale.getDate();
+        seller = new SellerDTO(sale.getSeller());
+    }
+}
